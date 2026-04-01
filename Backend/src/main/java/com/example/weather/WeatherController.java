3386614c4a75362150e44d14e6a35802afc8a9ae
segg3 <damian.segginger@coop.ch>
2026-04-01 16:09:20 +0200

    Set allowed cors origin (life server port) and set life server port in settings.json

--- Backend/src/main/java/com/example/weather/WeatherController.java
+++ Backend/src/main/java/com/example/weather/WeatherController.java
@@ -1,5 +1,6 @@
 package com.example.weather;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,8 @@ import com.example.weather.DTO.WeatherDTO;
 import com.example.weather.DTO.WeeklyDTO;
 
 @RestController
+// Life-Server FE
+@CrossOrigin(origins = "http://127.0.0.1:5501")
 @RequestMapping("api/v1/")
 public class WeatherController {
     private WeatherService service;
